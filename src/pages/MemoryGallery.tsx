694c68933e0ee95413a8624abb963c0165c25d67
ritohp <ritohp@gmail.com>
2026-04-19 18:32:36 -0600

    Sync Boutique 19/04/2026 18:32:36.49

--- src/pages/MemoryGallery.tsx
+++ src/pages/MemoryGallery.tsx
@@ -120,6 +120,3 @@ export default function MemoryGallery() {
   );
 }
 
-function ArrowRight({ size, className }: { size: number, className: string }) {
-    return <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className={className}><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>;
-}
