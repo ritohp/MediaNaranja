7583f4a8436a79acf19ebe2ecf172cc044f61014
ritohp <ritohp@gmail.com>
2026-05-17 18:45:57 -0600

    Sync Boutique 17/05/2026 18:45:57.75

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Vercel no tiene las variables, así que las quemamos por ahora
+const supabaseUrl = 'https://hkakmdpqbbsstacpgpqe.supabase.co';
+const supabaseAnonKey = 'sb_publishable_BI9Kc4Ik9F6ywN4ZjV6xiQ_y0iE4tbb';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
