5a220946bc7b4767dbf6764e636e5de5c64a8db0
ritohp <ritohp@gmail.com>
2026-05-17 19:09:27 -0600

    Sync Boutique 17/05/2026 19:09:27.42

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Conexión forzada a la base de datos activa "kwefdakzfmhuiqjylmuk"
-const supabaseUrl = 'https://kwefdakzfmhuiqjylmuk.supabase.co';
-const supabaseAnonKey = 'sb_publishable_nfZfVvuXGmHTqKd9LL6dGg_VEAByZaw';
+// Conexión forzada a la base de datos original y correcta "hkakmdpqbbsstacpgpqe"
+const supabaseUrl = 'https://hkakmdpqbbsstacpgpqe.supabase.co';
+const supabaseAnonKey = 'sb_publishable_BI9Kc4Ik9F6ywN4ZjV6xiQ_y0iE4tbb';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
