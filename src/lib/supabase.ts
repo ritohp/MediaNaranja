a29368d44422b5d5cfe3195370ef4dbb9ca6ca18
ritohp <ritohp@gmail.com>
2026-05-17 18:55:39 -0600

    Sync Boutique 17/05/2026 18:55:39.13

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Vercel no tiene las variables, así que las quemamos por ahora
-const supabaseUrl = 'https://hkakmdpqbbsstacpgpqe.supabase.co';
-const supabaseAnonKey = 'sb_publishable_BI9Kc4Ik9F6ywN4ZjV6xiQ_y0iE4tbb';
+// Conexión forzada a la base de datos activa "kwefdakzfmhuiqjylmuk"
+const supabaseUrl = 'https://kwefdakzfmhuiqjylmuk.supabase.co';
+const supabaseAnonKey = 'sb_publishable_nfZfVvuXGmHTqKd9LL6dGg_VEAByZaw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
